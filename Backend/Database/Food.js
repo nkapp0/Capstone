69c6e9b81f26f78c1fdb335588c8988b39df2a16
HunterB-74 <70162924+HunterB-74@users.noreply.github.com>
2024-04-29 22:43:22 -0500

    Updated Food.js with image links and cleaned up entries

--- Backend/Database/Food.js
+++ Backend/Database/Food.js
@@ -15,9 +15,9 @@
     "Link": "https://www.internationaltaphouse.com/",
     "Description": "Beer hall with massive selection of draft & bottled brews in a spacious, industrial-chic setting",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
-    "Rank": 0,
-    "Photo": "Placeholder"
+    "Tag2": "",
+    "Rank": 6,
+    "Photo": "../Views/FoodImg/taphouse.jpg"
     },
     {
     "_id": 3,
@@ -26,8 +26,8 @@
     "Description": "Loud, longtime college bar & grill offering beer, American grub & sports on TV, plus outdoor areas",
     "Tag1": "Bar",
     "Tag2": "Food",
-    "Rank": 0,
-    "Photo": "Placeholder"
+    "Rank": 12,
+    "Photo": "../Views/FoodImg/harpos.jpg"
     },
     {
     "_id": 4,
@@ -37,7 +37,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/fieldhouse.jpg"
     },
     {
     "_id": 5,
@@ -57,7 +57,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/shiloh.jpg"
     },
     {
     "_id": 7,
@@ -65,9 +65,9 @@
     "Link": "https://www.silverballbar.com/",
     "Description": "Serves great cocktails · Has dancing · Has bar games",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
+    "Tag2": "",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/silverball.jpg"
     },
     {
     "_id": 8,
@@ -77,7 +77,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/shotbar.jpg"
     },
     {
     "_id": 9,
@@ -95,7 +95,7 @@
     "Link": "https://ontherockscomo.com/",
     "Description": "Serves great cocktails",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
+    "Tag2": "",
     "Rank": 0,
     "Photo": "Placeholder"
     },
@@ -127,7 +127,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/flatbranchbrew.jpg"
     },
     {
     "_id": 14,
@@ -135,9 +135,9 @@
     "Link": "https://2trops.com/",
     "Description": "Frozen cocktails",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
+    "Tag2": "",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/tropicalliq.jpg"
     },
     {
     "_id": 15,
@@ -145,9 +145,9 @@
     "Link": "https://www.visitcolumbiamo.com/directory/eastside-tavern/",
     "Description": "Quaint alternative bar and nightclub",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
-    "Rank": 0,
-    "Photo": "Placeholder"
+    "Tag2": "",
+    "Rank": 12,
+    "Photo": "../Views/FoodImg/eastside.jpg"
     },
     {
     "_id": 16,
@@ -155,7 +155,7 @@
     "Link": "https://www.facebook.com/p/KlikS-Deli-Bar-100063536863308/",
     "Description": "Has outdoor seating · Serves great cocktails",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
+    "Tag2": "",
     "Rank": 0,
     "Photo": "Placeholder"
     },
@@ -181,13 +181,13 @@
     },
     {
     "_id": 19,
-    "Name": "The Penguin Piano Bar (2024 return) Need more Research",
-    "Link": "Placeholder",
-    "Description": "Placeholder",
-    "Tag1": "Placeholder",
-    "Tag2": "Placeholder",
+    "Name": "The Penguin Piano Bar",
+    "Link": "https://www.penguincomo.com",
+    "Description": "Enjoy live music in this bar & restaurant located in the heart of downtown.",
+    "Tag1": "Bar",
+    "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/penguinbar.jpg"
     },
     {
     "_id": 20,
@@ -217,7 +217,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/broadwayBrewery.jpg"
     },
     {
     "_id": 23,
@@ -245,7 +245,7 @@
     "Link": "https://www.mcgintysbar.com/",
     "Description": "Outside food allowed · Has outdoor seating",
     "Tag1": "Alcohol",
-    "Tag2": "Placeholder",
+    "Tag2": "",
     "Rank": 0,
     "Photo": "Placeholder"
     },
@@ -257,7 +257,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/divebar.jpg"
     },
     {
     "_id": 27,
@@ -267,7 +267,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/lasmargaritas.jpg"
     },
     {
     "_id": 28,
@@ -277,7 +277,7 @@
     "Tag1": "Bar",
     "Tag2": "Food",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/nashvegas.jpg"
     },
     {
     "_id": 29,
@@ -297,7 +297,7 @@
     "Tag1": "Bar",
     "Tag2": "Burgers",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/booches.jpg"
     },
     {
     "_id": 31,
@@ -306,7 +306,7 @@
     "Description": "Serves Cocktails and has bar games",
     "Tag1": "Alcohol",
     "Rank": 0,
-    "Photo": "Placeholder"
+    "Photo": "../Views/FoodImg/snappers.jpg"
     },
     
     {
@@ -377,16 +377,6 @@
     "Rank": 0,
     "Photo": "Placeholder"
     },
-    
-    {
-    "_id": 39,
-    "Name": "Panda Express - Student Center",
-    "Link": "https://dining.missouri.edu/locations/panda-express/",
-    "Description": "American - Chinese food",
-    "Tag1": "Chinese",
-    "Rank": 0,
-    "Photo": "Placeholder"
-        },
     {
     "_id": 40,
     "Name": "Plaza 900",
@@ -433,34 +423,8 @@
     "Link": "https://dining.missouri.edu/locations/sabai/",
     "Description": "Southeast Asian fusion serving ramen, rice bowls, tacos, etc.",
     "Tag1": "Asian",
-    "Tag2": ""
-  },
-  {
-    "_id": 45,
-    "Name": "Starbucks (Memorial) - Memorial Student Union",
-    "Rank": 0,
-    "Link": "https://dining.missouri.edu/locations/starbucks-memorial-union/",
-    "Description": "A Starbucks located in the Memorial Student Union Building",
-    "Tag1": "Coffee",
-    "Tag2": ""
-  },
-  {
-    "_id": 46,
-    "Name": "Starbucks (Southwest)",
-    "Rank": 0,
-    "Link": "https://dining.missouri.edu/locations/starbucks-southwest/",
-    "Description": "A Starbucks located in the Southwest",
-    "Tag1": "Coffee",
-    "Tag2": ""
-  },
-  {
-    "_id": 47,
-    "Name": "Subway (Southwest)",
-    "Rank": 0,
-    "Link": "https://dining.missouri.edu/locations/subway-southwest/",
-    "Description": "A small subway located within the Southwest Dining Hall",
-    "Tag1": "",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 48,
@@ -469,16 +433,8 @@
     "Link": "https://dining.missouri.edu/locations/sunshine-sushi/",
     "Description": "Walk-up sushi with grab and go sushi rolls and sides",
     "Tag1": "Sushi",
-    "Tag2": ""
-  },
-  {
-    "_id": 49,
-    "Name": "The Mark on 5th Street - Mark Twain Hall",
-    "Rank": 0,
-    "Link": "",
-    "Description": "",
-    "Tag1": "",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 50,
@@ -487,7 +443,8 @@
     "Link": "https://dining.missouri.edu/locations/truffles/",
     "Description": "Bakery serving breakfast and dessert as well as smoothies",
     "Tag1": "Bakery",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 51,
@@ -496,16 +453,8 @@
     "Link": "https://dining.missouri.edu/locations/wheatstone-bistro/",
     "Description": "Sandwich shop specializing in breakfast and lunch. Also a nice place to grab a soup or salad on campus",
     "Tag1": "Lunch",
-    "Tag2": ""
-  },
-  {
-    "_id": 52,
-    "Name": "Restaurants at Southwest",
-    "Rank": 0,
-    "Link": "",
-    "Description": "",
-    "Tag1": "",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
     {
     "_id": 53,
@@ -514,7 +463,8 @@
     "Link": "https://shakespeares.com/",
     "Description": "Most famous pizza spot in Columbia, has a bar as well",
     "Tag1": "Pizza",
-    "Tag2": "Bar"
+    "Tag2": "Bar",
+    "Photo": "../Views/FoodImg/shakespeares.jpg"
   },
   {
     "_id": 54,
@@ -523,7 +473,8 @@
     "Link": "https://www.pizzatreepizza.com/",
     "Description": "Walk up and pick up from window",
     "Tag1": "Pizza",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/pizzatree.jpg"
   },
   {
     "_id": 55,
@@ -532,16 +483,8 @@
     "Link": "https://www.facebook.com/people/Tonys-Pizza-Palace-Columbia/100063614801407/",
     "Description": "Laid-back institution serving up pizza, subs & Greek standards, plus wine & beer",
     "Tag1": "Pizza",
-    "Tag2": "Bar"
-  },
-  {
-    "_id": 56,
-    "Name": "Flat Branch",
-    "Rank": 0,
-    "Link": "https://www.flatbranch.com/",
-    "Description": "House brewed beer and American pub food",
-    "Tag1": "Pub Food",
-    "Tag2": "Bar"
+    "Tag2": "Bar",
+    "Photo": "../Views/FoodImg/tonyspizza.jpg"
   },
   {
     "_id": 57,
@@ -550,7 +493,8 @@
     "Link": "https://addisonsgrill.com/",
     "Description": "American grill located in the heart of downtown",
     "Tag1": "Dinner",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/addisons.jpg"
   },
   {
     "_id": 58,
@@ -559,7 +503,8 @@
     "Link": "https://theheidelberg.com/",
     "Description": "Comfort food, includes a bar and an outdoor seating area upstairs",
     "Tag1": "Comfort Food",
-    "Tag2": "Bar"
+    "Tag2": "Bar",
+    "Photo": "Placeholder"
   },
   {
     "_id": 59,
@@ -568,7 +513,8 @@
     "Link": "https://www.facebook.com/budsclassicbbq/",
     "Description": "Classic BBQ spot with outdoor seating",
     "Tag1": "Barbeque",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/buds.jpg"
   },
   {
     "_id": 60,
@@ -577,7 +523,8 @@
     "Link": "https://www.cjshotwings.com/",
     "Description": "Repeatedly voted best wings in Columbia and also includes sandwiches and beer",
     "Tag1": "Wings/Chicken",
-    "Tag2": "Sandwiches"
+    "Tag2": "Sandwiches",
+    "Photo": "../Views/FoodImg/cjs.jpg"
   },
   {
     "_id": 61,
@@ -586,7 +533,8 @@
     "Link": "https://www.broadwaydinercomo.com/",
     "Description": "Small, classic American diner that includes breakfast and lunch",
     "Tag1": "Breakfast",
-    "Tag2": "Lunch"
+    "Tag2": "Lunch",
+    "Photo": "../Views/FoodImg/broadwayDiner.jpg"
   },
   {
     "_id": 62,
@@ -595,7 +543,8 @@
     "Link": "https://erniescolumbia.com/",
     "Description": "Diner style spot that includes breakfast, lunch, and dinner",
     "Tag1": "Dinner",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/ernies.jpg"
   },
   {
     "_id": 63,
@@ -604,7 +553,8 @@
     "Link": "https://www.cafeberlincomo.com/",
     "Description": "Hipster vibe spot with live music in the evenings",
     "Tag1": "Breakfast",
-    "Tag2": "Lunch"
+    "Tag2": "Lunch",
+    "Photo": "Placeholder"
   },
   {
     "_id": 64,
@@ -613,7 +563,8 @@
     "Link": "https://www.elosocomo.com/",
     "Description": "Mexican grill with a variety of options, as well as Margs",
     "Tag1": "Mexican",
-    "Tag2": "Margs"
+    "Tag2": "Margs",
+    "Photo": "../Views/FoodImg/eloso.jpg"
   },
   {
     "_id": 65,
@@ -622,7 +573,8 @@
     "Link": "https://room-38.com/",
     "Description": "Variety of foods, as well as a solid bar and nightlife scene",
     "Tag1": "Lunch",
-    "Tag2": "Bar"
+    "Tag2": "Bar",
+    "Photo": "Placeholder"
   },
   {
     "_id": 66,
@@ -631,7 +583,8 @@
     "Link": "https://www.44canteen.com/",
     "Description": "Dynamic comfort food with a bar included",
     "Tag1": "Pub Food",
-    "Tag2": "Bar"
+    "Tag2": "Bar",
+    "Photo": "Placeholder"
   },
   {
     "_id": 67,
@@ -640,7 +593,8 @@
     "Link": "https://sparkyshomemadeicecream.wordpress.com/",
     "Description": "Cozy ice cream spot serving a variety of homemade flavors",
     "Tag1": "Dessert",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/sparkys.jpg"
   },
   {
     "_id": 68,
@@ -649,7 +603,8 @@
     "Link": "https://www.facebook.com/elranchocomo/",
     "Description": "Fast, authentic Mexican restaurant with a variety of options",
     "Tag1": "Mexican",
-    "Tag2": "Margs"
+    "Tag2": "Margs",
+    "Photo": "../Views/FoodImg/elrancho.jpg"
   },
   {
     "_id": 69,
@@ -658,7 +613,8 @@
     "Link": "https://www.seoultaco.com/mizzou",
     "Description": "Korean-Mexican hybrid restaurant",
     "Tag1": "Tacos",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/seoultaco.jpg"
   },
   {
     "_id": 70,
@@ -667,7 +623,8 @@
     "Link": "https://billiardsonbroadway.com/",
     "Description": "Huge tvs, pool tables, and features what some call the best burgers and fries in Columbia",
     "Tag1": "Burgers",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "../Views/FoodImg/billards.jpg"
   },
   {
     "_id": 71,
@@ -676,7 +633,8 @@
     "Link": "https://www.bbbagelco.com/",
     "Description": "Authentic New York Style bagel shop",
     "Tag1": "Breakfast",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 72,
@@ -685,7 +643,8 @@
     "Link": "https://www.tropicalsmoothiecafe.com/",
     "Description": "Smoothie cafe with a variety of food options as well",
     "Tag1": "Smoothies",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 73,
@@ -694,7 +653,8 @@
     "Link": "https://www.uprisebakery.com/",
     "Description": "Bakery with plenty of breakfast and lunch options, as well as a bar",
     "Tag1": "Breakfast/Lunch",
-    "Tag2": "Bakery, Bar"
+    "Tag2": "Bakery, Bar",
+    "Photo": "Placeholder"
   },
   {
     "_id": 74,
@@ -703,7 +663,8 @@
     "Link": "https://www.ordergoldies.com/",
     "Description": "Warm and comforting bagel spot with over 15 different bagel options",
     "Tag1": "Breakfast",
-    "Tag2": "Bagels"
+    "Tag2": "Bagels",
+    "Photo": "Placeholder"
   },
   {
     "_id": 75,
@@ -712,7 +673,8 @@
     "Link": "http://www.main-squeeze.com/",
     "Description": "Juice and smoothie bar",
     "Tag1": "Smoothies",
-    "Tag2": ""
+    "Tag2": "",
+    "Photo": "Placeholder"
   },
   {
     "_id": 76,
@@ -721,7 +683,8 @@
     "Link": "https://www.facebook.com/p/Cafe-Poland-100037248802901/",
     "Description": "Family owned Polish cafe with plenty of Polish dishes",
     "Tag1": "Polish Food",
-    "Tag2": "Cafe"
+    "Tag2": "Cafe",
+    "Photo": "../Views/FoodImg/cafepoland.jpg"
   },
   {
     "_id": 77,
@@ -730,7 +693,8 @@
     "Link": "https://www.hotboxcookies.com/",
     "Description": "Warm cookies and ice cream",
     "Tag1": "Cookies",
-    "Tag2": "Ice Cream"
+    "Tag2": "Ice Cream",
+    "Photo": "../Views/FoodImg/hotbox.jpg"
   },
     {
       "_id": 78,
@@ -739,7 +703,8 @@
       "Link": "https://justlovecoffeecafe.com/mo-columbia-battle-crossing/",
       "Description": "Locally owned and operated coffee shop serving freshly brewed coffee and freshly baked goods. Also serving breakfast, lunch, and dinner all day.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 79,
@@ -748,7 +713,8 @@
       "Link": "",
       "Description": "Cozy and inviting coffee shop born in Columbia, MO, located right downtown offering easy access to artisan coffee.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "../Views/FoodImg/shortwave.jpg"
   },
   {
       "_id": 80,
@@ -757,7 +723,8 @@
       "Link": "https://lakotacoffee.com/",
       "Description": "Lakota coffee roasts their beans in house for an authentic and topnotch brew. Additionally, they serve baked goods sourced from the phenomenal Uprise Bakery next door.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "../Views/FoodImg/lakota.jpg"
   },
   {
       "_id": 81,
@@ -766,7 +733,8 @@
       "Link": "http://www.vidacoffeeco.com/",
       "Description": "Vida Coffee is an independently owned and operated coffee shop conveniently located across the street from Student Rec Center and Virginia Ave parking garage. Vida Coffee offers free wifi and a relaxing place for students to chill and study as well as enjoy delicious coffee.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 82,
@@ -775,7 +743,8 @@
       "Link": "https://www.fretboardcoffee.com/",
       "Description": "Fretboard Coffee is a specialty coffee shop that also offers delivery and coffee subscriptions.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 83,
@@ -784,7 +753,8 @@
       "Link": "https://www.thecolumbiagrind.com/",
       "Description": "The Grind Coffee House offers a study bar and lounge space with free wifi for students to get away from their usual study zones and enjoy a cup of coffee, smoothie, or one of their various lunch options.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 84,
@@ -793,7 +763,8 @@
       "Link": "https://lacalle8cafe.square.site/",
       "Description": "La Calle 8 is a vibrant and cozy coffee shop located in the heart of downtown Columbia where customers can enjoy a fine cup of coffee or a light sandwich.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 85,
@@ -802,7 +773,8 @@
       "Link": "http://www.acolacoffee.com/",
       "Description": "Acola coffee is a locally owned and operated coffee shop located in the North Village art district of Downtown Columbia. Acola serves coffee produced by Onyx coffee lab. While they do not currently have space to study and relax, they plan to expand soon.",
       "Tag1": "Coffee",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   },
   {
       "_id": 86,
@@ -811,6 +783,7 @@
       "Link": "https://www.facebook.com/p/Bobaholic-CoMo-100090077037568/",
       "Description": "Freshly brew bubble teas, smoothies, slushes, fruit teas, coffee, and banh mi sandwiches.",
       "Tag1": "Cafe",
-      "Tag2": ""
+      "Tag2": "",
+      "Photo": "Placeholder"
   }
   ]
